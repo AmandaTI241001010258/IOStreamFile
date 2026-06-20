9f1a2610057678d913d3b6fc63c1fda4dc5eee17
AmandaTI241001010258 <2410010258manda@gmail.com>
2026-06-20 15:59:29 +0800

    Selesai Bagian 2: Baca Tulis Berkas

---
+++ src/bagian2/bacatulis/LatihanMandiri3.java
@@ -0,0 +1,26 @@
+package bagian2.bacatulis;
+
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.IOException;
+
+public class LatihanMandiri3 {
+    public static void main(String[] args) {
+
+        int jumlahBaris = 0;
+
+        try (BufferedReader pembaca =
+                new BufferedReader(new FileReader("hari.txt"))) {
+
+            while (pembaca.readLine() != null) {
+                jumlahBaris++;
+            }
+
+            System.out.println("Jumlah baris dalam hari.txt = "
+                    + jumlahBaris);
+
+        } catch (IOException e) {
+            System.out.println("Gagal membaca: " + e.getMessage());
+        }
+    }
+}
